3c55fcfe76be66ed6e6ed8bb25ce9070e1cdd007
deal-machine <95deal@gmail.com>
2023-03-24 21:51:38 -0300

    read books into buttons

--- App.tsx
+++ App.tsx
@@ -1,20 +1,44 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  ScrollView,
+  SafeAreaView,
+  Button,
+  Alert,
+} from "react-native";
+
+import _bible from "./bible.json";
+import { Book } from "./src/types/Book";
 
 export default function App() {
+  const bible = _bible as Book[];
+  const books = bible.map((book: Book) => {
+    const versiculo = book.chapters[0];
+    return (
+      <Button
+        key={book.abbrev}
+        title={book.name}
+        onPress={() => Alert.alert(`${versiculo}`)}
+      />
+    );
+  });
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        {books}
+        <StatusBar style="auto" />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // width: "100%",
+    // paddingVertical: 50,
+    padding: 20,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-around",
   },
 });
